c73723de44e87979327697f25b6ad13ce6c8abae
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-08-06 09:50:26 -0300

    Lá vamos nós de novo...

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+}
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, Roboto, 'Helvetica Neue',
+    'Segoe UI', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
