15405736be461082b71025c7e8fed81847bf2f8c
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-08-06 11:24:34 -0300

    Estruturação do layout com Header

---
+++ components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+    <nav>
+      <a
+        v-for="n in 3"
+        :key="n"
+        :href="current === n ? null : `/${n}`"
+        :class="{ 'exact-active': current === n }"
+        >Example {{ n }}</a
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  computed: {
+    current() {
+      return parseInt(this.$route.path.replace(/[^\d]/g, ''), 10)
+    },
+  },
+}
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  text-align: center;
+  gap: 0.25rem;
+  height: 3rem;
+}
+
+a {
+  padding: 0.5rem;
+  text-decoration: none;
+  color: #00b7ff;
+  transition: all 0.25s ease-in-out;
+  outline: 1px solid #00b7ff00;
+}
+
+a:hover {
+  color: #6fd1ff;
+}
+
+a:focus {
+  outline-color: #00b7ff80;
+}
+
+a.exact-active {
+  cursor: default;
+  color: #00b7ff20;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <Nuxt />
+  <div id="app">
+    <Header />
+
+    <main>
+      <Nuxt />
+    </main>
   </div>
 </template>
 
@@ -27,5 +31,33 @@ html {
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
+  background: #151515;
+}
+
+#app {
+  min-height: 100vh;
+  padding: 1rem;
+  display: flex;
+  flex-flow: column;
+}
+
+main {
+  flex: 1;
+  border-radius: 4px;
+}
+
+main > section > div {
+  flex: 1;
+  width: 100%;
+  height: calc(100vh - 5rem);
+}
+
+main canvas {
+  border-radius: 4px;
+  box-shadow: 0 2px 6px #000;
+}
+
+main > section > div > *:first-child {
+  line-height: 0;
 }
 </style>
