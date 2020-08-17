f489fce50e61db0df11a4e9c4f153cf777d74cd4
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-08-17 08:14:30 -0300

    Avoid errors during router.push

--- pages/1.vue
+++ pages/1.vue
@@ -3,7 +3,8 @@
     <vgl-renderer scene="s" camera="c" antialias>
       <vgl-scene name="s" background-color="#111">
         <vgl-box-geometry name="g" />
-        <vgl-mesh geometry="g" />
+        <vgl-mesh-standard-material name="m" />
+        <vgl-mesh geometry="g" material="m" />
       </vgl-scene>
       <vgl-perspective-camera name="c" orbit-position="3 1 0.5" />
     </vgl-renderer>
@@ -15,6 +16,7 @@ import {
   VglRenderer,
   VglScene,
   VglBoxGeometry,
+  VglMeshStandardMaterial,
   VglMesh,
   VglPerspectiveCamera,
 } from 'vue-gl'
@@ -25,6 +27,7 @@ export default {
     VglRenderer,
     VglScene,
     VglBoxGeometry,
+    VglMeshStandardMaterial,
     VglMesh,
     VglPerspectiveCamera,
   },
